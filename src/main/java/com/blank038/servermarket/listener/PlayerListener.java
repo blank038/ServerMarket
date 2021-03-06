4036e9c8fa6eb12a1c0d9c4a5440243a68d3924f
Blank038 <admin@mc9y.com>
2021-03-06 21:19:05 +0800

    # 修改 onJoin 检测数据存在的一个隐性问题

--- src/main/java/com/blank038/servermarket/listener/PlayerListener.java
+++ src/main/java/com/blank038/servermarket/listener/PlayerListener.java
@@ -33,29 +33,7 @@ public class PlayerListener implements Listener {
         Player player = event.getPlayer();
         PlayerData.PLAYER_DATA.put(event.getPlayer().getUniqueId(), new PlayerData(player.getUniqueId()));
         // 判断是否有可获得的货币
-        Bukkit.getScheduler().runTaskAsynchronously(ServerMarket.getInstance(), () -> {
-            Set<String> keys = new HashSet<>();
-            for (Map.Entry<String, ResultData> entry : ResultData.RESULT_DATA.entrySet()) {
-                // 获取市场数据
-                MarketData marketData = MarketData.MARKET_DATA.getOrDefault(entry.getValue().getSourceMarket(), null);
-                // 获取可获得货币
-                double price = entry.getValue().getResultAmount(),
-                        last = marketData == null ? price : marketData.getLastMoney(player, price);
-                // 判断货币桥是否存在
-                if (BaseBridge.PAY_TYPES.containsKey(entry.getValue().getPayType())) {
-                    DecimalFormat df = new DecimalFormat("#0.00");
-                    ServerMarket.getInstance().getEconomyBridge(entry.getValue().getPayType()).give(event.getPlayer(), null, last);
-                    event.getPlayer().sendMessage(LangConfiguration.getString("sale-sell", true)
-                            .replace("%economy%", marketData == null ? "" : marketData.getDisplayName())
-                            .replace("%money%", df.format(price)).replace("%last%", df.format(last)));
-                    keys.add(entry.getKey());
-                }
-            }
-            // 不通过 new HashMap 来执行, 避免过度浪费性能
-            for (String key : keys) {
-                ResultData.RESULT_DATA.remove(key);
-            }
-        });
+        Bukkit.getScheduler().runTaskAsynchronously(ServerMarket.getInstance(), () -> this.checkResult(player));
     }
 
     /**
@@ -75,6 +53,30 @@ public class PlayerListener implements Listener {
                 event.setCancelled(true);
                 break;
             }
+        }
+    }
+
+    private synchronized void checkResult(Player player) {
+        Set<String> keys = new HashSet<>();
+        for (Map.Entry<String, ResultData> entry : ResultData.RESULT_DATA.entrySet()) {
+            // 获取市场数据
+            MarketData marketData = MarketData.MARKET_DATA.getOrDefault(entry.getValue().getSourceMarket(), null);
+            // 获取可获得货币
+            double price = entry.getValue().getResultAmount(),
+                    last = marketData == null ? price : marketData.getLastMoney(player, price);
+            // 判断货币桥是否存在
+            if (BaseBridge.PAY_TYPES.containsKey(entry.getValue().getPayType())) {
+                DecimalFormat df = new DecimalFormat("#0.00");
+                ServerMarket.getInstance().getEconomyBridge(entry.getValue().getPayType()).give(player, null, last);
+                player.sendMessage(LangConfiguration.getString("sale-sell", true)
+                        .replace("%economy%", marketData == null ? "" : marketData.getDisplayName())
+                        .replace("%money%", df.format(price)).replace("%last%", df.format(last)));
+                keys.add(entry.getKey());
+            }
+        }
+        // 不通过 new HashMap 来执行, 避免过度浪费性能
+        for (String key : keys) {
+            ResultData.RESULT_DATA.remove(key);
         }
     }
 }
