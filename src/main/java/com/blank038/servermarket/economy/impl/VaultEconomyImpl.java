b370c8f2f2400a41c071c21ea1725c79dc521556
Blank038 <admin@mc9y.com>
2023-11-25 11:38:58 +0800

    feat: Add logic check for Vault

--- src/main/java/com/blank038/servermarket/economy/impl/VaultEconomyImpl.java
+++ src/main/java/com/blank038/servermarket/economy/impl/VaultEconomyImpl.java
@@ -5,31 +5,55 @@ import com.blank038.servermarket.enums.PayType;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.plugin.RegisteredServiceProvider;
 
 /**
  * @author Blank038
  */
 @SuppressWarnings(value = {"unused"})
 public class VaultEconomyImpl extends BaseEconomy {
-    private final Economy economyProvider;
+    private Economy economyProvider;
 
     public VaultEconomyImpl() {
         super(PayType.VAULT);
-        economyProvider = Bukkit.getServicesManager().getRegistration(Economy.class).getProvider();
+        this.checkEconomy();
+    }
+
+    private void checkEconomy() {
+        if (this.economyProvider != null) {
+            return;
+        }
+        RegisteredServiceProvider<Economy> service = Bukkit.getServicesManager().getRegistration(Economy.class);
+        if (service == null) {
+            return;
+        }
+        this.economyProvider = service.getProvider();
     }
 
     @Override
     public double balance(OfflinePlayer player, String key) {
+        this.checkEconomy();
+        if (this.economyProvider == null) {
+            return 0.0D;
+        }
         return economyProvider.getBalance(player);
     }
 
     @Override
     public void give(OfflinePlayer player, String key, double amount) {
+        this.checkEconomy();
+        if (this.economyProvider == null) {
+            return;
+        }
         economyProvider.depositPlayer(player, Math.max(1, amount));
     }
 
     @Override
     public boolean take(OfflinePlayer player, String key, double amount) {
+        this.checkEconomy();
+        if (this.economyProvider == null) {
+            return false;
+        }
         return economyProvider.withdrawPlayer(player, Math.max(1, amount)).transactionSuccess();
     }
 }
