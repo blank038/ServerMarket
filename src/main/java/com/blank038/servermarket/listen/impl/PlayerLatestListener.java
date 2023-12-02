a202bc411284a21abfe496eeb27bfa209034609b
Blank038 <admin@mc9y.com>
2023-12-02 16:20:36 +0800

    perf: Optimize PlayerLatestListener logic

--- src/main/java/com/blank038/servermarket/listen/impl/PlayerLatestListener.java
+++ src/main/java/com/blank038/servermarket/listen/impl/PlayerLatestListener.java
@@ -3,14 +3,12 @@ package com.blank038.servermarket.listen.impl;
 import com.blank038.servermarket.data.DataContainer;
 import com.blank038.servermarket.listen.AbstractListener;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandSendEvent;
 
 /**
  * @author Blank038
  */
-public class PlayerLatestListener extends AbstractListener
-        implements Listener {
+public class PlayerLatestListener extends AbstractListener {
 
     @EventHandler
     public void onPlayerCommandSend(PlayerCommandSendEvent event) {
