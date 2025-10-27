4f5ac898f504cbe1413d742c27e2bf41b13408cd
Blank038 <admin@mc9y.com>
2025-10-27 21:45:32 +0800

    refactor: Move INotifyService#broadcast logic to AbstractNotifyService

--- bukkit/src/main/java/com/blank038/servermarket/internal/service/notify/INotifyService.java
+++ bukkit/src/main/java/com/blank038/servermarket/internal/service/notify/INotifyService.java
@@ -1,7 +1,6 @@
 package com.blank038.servermarket.internal.service.notify;
 
 import com.blank038.servermarket.internal.cache.other.NotifyCache;
-import org.bukkit.Bukkit;
 import org.bukkit.configuration.ConfigurationSection;
 
 /**
@@ -13,7 +12,5 @@ public interface INotifyService {
 
     void push(NotifyCache notifyCache);
 
-    default void broadcast(String message) {
-        Bukkit.getOnlinePlayers().forEach((player) -> player.sendMessage(message));
-    }
+    void broadcast(String message);
 }
--- bukkit/src/main/java/com/blank038/servermarket/internal/service/notify/impl/AbstractNotifyService.java
+++ bukkit/src/main/java/com/blank038/servermarket/internal/service/notify/impl/AbstractNotifyService.java
@@ -1,9 +1,15 @@
 package com.blank038.servermarket.internal.service.notify.impl;
 
 import com.blank038.servermarket.internal.service.notify.INotifyService;
+import org.bukkit.Bukkit;
 
 /**
  * @author Blank038
  */
 public abstract class AbstractNotifyService implements INotifyService {
+
+    @Override
+    public void broadcast(String message) {
+        Bukkit.getOnlinePlayers().forEach((player) -> player.sendMessage(message));
+    }
 }
